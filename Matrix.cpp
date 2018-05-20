d0573e29f8dda9ca5af44dab8c7c3ba7f05f6cd7
AriosJentu <darthpoezd@gmail.com>
2018-05-20 22:36:29 +1000

    Slices

--- Matrix.cpp
+++ Matrix.cpp
@@ -15,21 +15,18 @@ template <class TypeM>
 void Matrix<TypeM>::transpose() {
 
 	matrix->isTransposed = not matrix->isTransposed;
-
-	/*int tmp = width;
-	width = height;
-	height = tmp;*/
-
 	std::swap(matrix->width, matrix->height);
 }
 
 template <class TypeM>
 int Matrix<TypeM>::index(int i, int j) {
 
+	// std::cout << "Debug: " << i << " " << j << " : " << matrix << " " << matrix->sheight << " " << matrix->swidth << std::endl;
 	if (not matrix->isTransposed) {
 		
 		if (i >= matrix->sheight or j >= matrix->swidth or i < 0 or j < 0) {
-			throw Exception("Wrong indexes");
+			// std::cout << "NOT TRANSPOSED " << (i >= matrix->sheight) << " " << (j >= matrix->swidth) << " : " << i << " " << j << " " << matrix->sheight << " " << matrix->swidth << " ENDL" << std::endl;
+			throw Exception("Wrong indexes ");
 			return -1;
 		}
 
@@ -87,7 +84,7 @@ Matrix<TypeM> Matrix<TypeM>::sum(Matrix mt1, Matrix mt2) {
 	h2 = mt2.matrix->height;
 
 
-	if (not (w1 == w2) and not (h1 == h2)) {
+	if (not (w1 == w2) or not (h1 == h2)) {
 		throw Exception("Can't sum matrixes with different size");
 		return mt1;
 	}
@@ -157,12 +154,14 @@ Matrix<TypeM> Matrix<TypeM>::mul(Matrix mt2) {
 }
 
 template <class TypeM>
-Matrix<TypeM> Matrix<TypeM>::mget(Matrix mt, int startY, int startX, int nheight, int nwidth) {
+Matrix<TypeM> Matrix<TypeM>::mget(Matrix mt, int startY, int startX, int nheight, int nwidth, int step) {
 	
-	if ((startX + nwidth) <= 0 or 
-		(startX + nwidth) > mt.matrix->width or 
-		(startY + nheight) <= 0 or 
-		(startY + nheight) > mt.matrix->height) 
+	if ((startX + (nwidth-1)*step) <= 0 or 
+		(startX + (nwidth-1)*step) > mt.matrix->width or 
+		(startY + (nheight-1)*step) <= 0 or 
+		(startY + (nheight-1)*step) > mt.matrix->height
+
+		or (step <= 0) )
 	{
 
 		throw Exception("Wrong matrix slice size");
@@ -171,10 +170,10 @@ Matrix<TypeM> Matrix<TypeM>::mget(Matrix mt, int startY, int startX, int nheight
 
 	Matrix mtn(nheight, nwidth);
 
-	for (int i = startY; i < startY + nheight; i++) {
-		for (int j = startX; j < startX + nwidth; j++) {
+	for (int i = startY; i < startY + nheight*step; i += step) {
+		for (int j = startX; j < startX + nwidth*step; j += step) {
 
-			mtn[i-startY][j-startX] = mt[i][j];
+			mtn[(i-startY)/step][(j-startX)/step] = mt[i][j];
 		}
 	}
 
@@ -182,7 +181,7 @@ Matrix<TypeM> Matrix<TypeM>::mget(Matrix mt, int startY, int startX, int nheight
 }
 
 template <class TypeM>
-Matrix<TypeM> Matrix<TypeM>::mget(int startY, int startX, int nheight, int nwidth) {
+Matrix<TypeM> Matrix<TypeM>::mget(int startY, int startX, int nheight, int nwidth, int step) {
 
 	Matrix mt(matrix->sheight, matrix->swidth);	
 	mt.matrix->arr = matrix->arr;
@@ -191,25 +190,27 @@ Matrix<TypeM> Matrix<TypeM>::mget(int startY, int startX, int nheight, int nwidt
 	mt.matrix->width = matrix->width;
 	mt.matrix->height = matrix->height;
 
-	return Matrix::mget(mt, startY, startX, nheight, nwidth);
+	return Matrix::mget(mt, startY, startX, nheight, nwidth, step);
 }
 
 template <class TypeM>
-Matrix<TypeM> Matrix<TypeM>::mset(Matrix mt1, Matrix mt2, int startY, int startX) {
+Matrix<TypeM> Matrix<TypeM>::mset(Matrix mt1, Matrix mt2, int startY, int startX, int step) {
 
-	if ((startX + mt2.matrix->width) <= 0 or 
-		(startX + mt2.matrix->width) > mt1.matrix->width or 
-		(startY + mt2.matrix->height) <= 0 or
-		(startY + mt2.matrix->height) > mt1.matrix->height)	
+	if (
+		step <= 0 or
+		(startX + (mt2.matrix->width-1)*step) <= 0 or 
+		(startX + (mt2.matrix->width-1)*step) > mt1.matrix->width or 
+		(startY + (mt2.matrix->height-1)*step) <= 0 or
+		(startY + (mt2.matrix->height-1)*step) > mt1.matrix->height)	
 	{
 
 		throw Exception("Can't put sliced matrix inside this matrix");
 		return mt1;
 	}
 
-	for (int i = startY; i < startY + mt2.matrix->height; i++) {
-		for (int j = startX; j < startX + mt2.matrix->width; j++) {
-			mt1[i][j] = mt2[i-startY][j-startX];
+	for (int i = startY; i < startY + mt2.matrix->height*step; i+=step) {
+		for (int j = startX; j < startX + mt2.matrix->width*step; j+=step) {
+			mt1[i][j] = mt2[(i-startY)/step][(j-startX)/step];
 		}
 	}
 
@@ -218,7 +219,7 @@ Matrix<TypeM> Matrix<TypeM>::mset(Matrix mt1, Matrix mt2, int startY, int startX
 }
 
 template <class TypeM>
-Matrix<TypeM> Matrix<TypeM>::mset(Matrix mt2, int startY, int startX) {
+Matrix<TypeM> Matrix<TypeM>::mset(Matrix mt2, int startY, int startX, int step) {
 
 	Matrix mt(matrix->sheight, matrix->swidth);	
 	mt.matrix->arr = matrix->arr;
@@ -227,8 +228,9 @@ Matrix<TypeM> Matrix<TypeM>::mset(Matrix mt2, int startY, int startX) {
 	mt.matrix->width = matrix->width;
 	mt.matrix->height = matrix->height;
 
-	return Matrix::mset(mt, mt2, startY, startX);
+	return Matrix::mset(mt, mt2, startY, startX, step);
 }
+
 
 
 
@@ -251,6 +253,8 @@ Matrix<TypeM>::Matrix(int mheight, int mwidth) {
 	matrix->arr = new TypeM[matrix->height*matrix->width];
 
 }
+
+
 
 template <class TypeM>
 Matrix<TypeM> operator+(Matrix<TypeM>& mt1, Matrix<TypeM>& mt2) {
@@ -277,19 +281,174 @@ Matrix<TypeM> operator*(Matrix<TypeM>& mt1, Matrix<TypeM>& mt2) {
 	return mt1.mul(mt2);
 }
 
+template <class TypeM>
+bool operator==(Matrix<TypeM> mt1, Matrix<TypeM> mt2) {
+
+	bool isValid = (
+		mt1.getWidth() == mt2.getWidth()
+	) and (
+		mt1.getHeight() == mt2.getHeight()
+	);
+
+	for (int i = 0; i < mt1.getHeight(); i++) {
+		for (int j = 0; j < mt1.getWidth(); j++) {
+			if (not isValid) {
+				return false;
+			} else {
+				isValid = mt1[i][j] == mt2[i][j];
+			}
+
+		}
+		if (not isValid) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 
 
 
+template <class TypeM>
+int Slice<TypeM>::getWidth() {
+	return width;
+}
+
+template <class TypeM>
+int Slice<TypeM>::getHeight() {
+	return height;
+}
+
+template <class TypeM>
+int Slice<TypeM>::getStepSize() {
+	return step;
+}
+ 
+ 
+template <class TypeM>
+int Slice<TypeM>::index(int i, int j) {
+	
+	if (i >= height or j >= width or i < 0 or j < 0) {
+		throw Exception("Wrong indexes");
+		return -1;
+	}
+	return matrix->index(startY + i*step, startX + j*step);
+}
+
+template <class TypeM>
+Matrix<TypeM> Slice<TypeM>::getAsMatrix() {
+	return matrix->mget(startY, startX, height, width, step);
+}
+
+template <class TypeM>
+Matrix<TypeM> Slice<TypeM>::getAsMatrix(Slice<TypeM> slc) {
+	return slc.getAsMatrix();
+}
+
+
+template <class TypeM>
+Slice<TypeM> Slice<TypeM>::mul(Slice<TypeM> slc, TypeM val) {
+	for (int i = 0; i < slc.getHeight(); i++) {
+		for (int j = 0; j < slc.getWidth(); j++) {
+			slc[i][j] = slc[i][j]*val;
+		}
+	}
+	return slc;
+}
+
+template <class TypeM>
+Slice<TypeM> Slice<TypeM>::mul(TypeM val) {
+		
+	Slice slc(matrix, startY, startX, height, width, step);	
+
+	return Slice::mul(slc, val);
+
+}
 
 
 
+template <class TypeM>
+Slice<TypeM>::Slice(Matrix<TypeM>* m, int nstartY, int nstartX, int sheight, int swidth, int nstep) {
+
+	matrix = m;
+	if ((nstartY + (sheight-1)*nstep >= matrix->getHeight()) or (nstartX + (swidth-1)*nstep >= matrix->getWidth())) {
+		throw Exception("Wrong matrix slice size");
+	}
+
+	startY = nstartY;
+	startX = nstartX;
+	height = sheight;
+	width = swidth;
+	step = nstep;
+}
+
+template <class TypeM>
+Slice<TypeM> operator*(Slice<TypeM>& slc, TypeM c) {
+	return slc.mul(c);
+}
 
 
 
-
+/*
 int main() {
+	Matrix<int> a(5, 5);
+	for (int i = 0; i < a.getHeight(); i++) {
+		for (int j = 0; j < a.getWidth(); j++) {
+			a[i][j] = i*a.getWidth() + j + 2;
+			std::cout << a[i][j] << ' ';
+		}
+		std::cout << std::endl;
+	}
 
+	std::cout << std::endl;
+
+	Matrix<int> k = a.mget(1, 0, 4, 4, 1);
+	for (int i = 0; i < k.getHeight(); i++) {
+		for (int j = 0; j < k.getWidth(); j++) {
+			std::cout << k[i][j] << ' ';
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+	Matrix<int> b(2, 2);
+
+	//a.mset(b, 0, 2, 2);
+	for (int i = 0; i < a.getHeight(); i++) {
+		for (int j = 0; j < a.getWidth(); j++) {
+			std::cout << a[i][j] << ' ';
+		}
+		std::cout << std::endl;
+	}
+
+	Slice<int> slc(&a, 0, 0, 3, 3, 2);
+	
+	for (int i = 0; i < slc.getHeight(); i++) {
+		for (int j = 0; j < slc.getWidth(); j++) {
+			slc[i][j] = 1;
+		}
+		
+	}
+	slc = slc*(-9);
+
+	std::cout << std::endl;
+	for (int i = 0; i < a.getHeight(); i++) {
+		for (int j = 0; j < a.getWidth(); j++) {
+			std::cout << a[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	Matrix<int> c = slc.getAsMatrix();
+
+}
+*/
+
+
+
+/*
+int main() {
 	//test1
 	Matrix<float> mt(3, 6);
 
@@ -497,4 +656,35 @@ int main() {
 		std::cout << std::endl;
 	}
 
+	Matrix<int> matrix(2, 3);
+
+	for (int i = 0; i < matrix.getHeight(); i++) {
+		for (int j = 0; j < matrix.getWidth(); j++) {
+
+			matrix[i][j] = i*matrix.getWidth() + j;
+		}
+	}
+
+	Matrix<int> matrix2(3, 2);
+	for (int i = 0; i < matrix2.getHeight(); i++) {
+		for (int j = 0; j < matrix2.getWidth(); j++) {
+			matrix2[i][j] = j*matrix2.getHeight() + i;
+		}
+	}
+
+	for (int i = 0; i < matrix.getHeight(); i++) {
+		for (int j = 0; j < matrix.getWidth(); j++) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	matrix2.transpose();
+	for (int i = 0; i < matrix2.getHeight(); i++) {
+		for (int j = 0; j < matrix2.getWidth(); j++) {
+			std::cout << matrix2[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
+*/
--- Matrix.h
+++ Matrix.h
@@ -3,6 +3,9 @@
 
 #pragma once
 
+//Proxy Slice + Slice Stepping
+
+
 template <class TypeM>
 struct MatrixLink {
 	
@@ -12,16 +15,67 @@ struct MatrixLink {
 	bool isTransposed = false;
 	int height, width;
 
+	~MatrixLink() {
+	}
+
 };
 
 template <class TypeM>
 class Matrix {
 private:
 
-	MatrixLink<TypeM>* matrix;
+	//MatrixLink<TypeM>* matrix;
+
+	void eventClear() {
+
+		if (matrix != NULL) {
+
+			matrix->counter--;
+			if (matrix->counter == 0) {
+				delete(matrix);				
+			}	
+
+			matrix = NULL;
+		}
+	}
+
+	void matrixMove(MatrixLink<TypeM>* frommatrix) {
+		
+		if (matrix != frommatrix) {
+
+			eventClear();
+
+			if (frommatrix != NULL) {
+
+				frommatrix->counter++;
+				matrix = frommatrix;
+			}
+		}
+	}
+
+	void matrixCopy(MatrixLink<TypeM>* frommatrix) {
+
+		if (matrix != frommatrix) {
+
+			MatrixLink<TypeM>* tmp;
+			int size = frommatrix->swidth * frommatrix->sheight;
+			
+			tmp->swidth = frommatrix->swidth;
+			tmp->sheight = frommatrix->sheight;
+			tmp->width = frommatrix->width;
+			tmp->height = frommatrix->height;
+			tmp->isTransposed = frommatrix->isTransposed;
+
+			memcpy(tmp->arr, frommatrix->arr, sizeof(TypeM)*size);
+
+			eventClear();
+			matrix = tmp;
+		}
+	}
 
 public:
 
+	MatrixLink<TypeM>* matrix;
 
 	void transpose();
 	int index(int i, int j);
@@ -29,20 +83,28 @@ public:
 	static Matrix cmul(Matrix mt, TypeM val);
 	static Matrix mul(Matrix mt1, Matrix mt2);
 	static Matrix sum(Matrix mt1, Matrix mt2);
-	static Matrix mget(Matrix mt, int startY, int startX, int nheight, int nwidth);
-	static Matrix mset(Matrix mt1, Matrix mt2, int startY, int startX);
+	static Matrix mget(Matrix mt, int startY, int startX, int nheight, int nwidth, int step=1);
+	static Matrix mset(Matrix mt1, Matrix mt2, int startY, int startX, int step=1);
 
 	Matrix cmul(TypeM val);
 	Matrix mul(Matrix mt);
 	Matrix sum(Matrix mt);
-	Matrix mget(int startY, int startX, int nheight, int nwidth);
-	Matrix mset(Matrix mt, int startY, int startX);
+	Matrix mget(int startY, int startX, int nheight, int nwidth, int step=1);
+	Matrix mset(Matrix mt, int startY, int startX, int step=1);
 
 	int getWidth();
 	int getHeight();
 
 	//construct
 	Matrix(int height, int width=1);
+
+	~Matrix() {
+
+		matrix->counter--;
+		if (matrix->counter < 0) {
+			//std::cout << "Matrix Links empty!" << std::endl;
+		}
+	}
 
 	//proxy class
 	class Proxy {
@@ -60,9 +122,6 @@ public:
 			return mtx->matrix->arr[mtx->index(mi, j)];
 		}
 
-		void operator=(TypeM val) {
-			mtx->matrix->arr[mi] = val;
-		}
 	};
 
 	//exception class
@@ -90,15 +149,77 @@ public:
 
 	Matrix operator=(Matrix mtx) {
 
-		if (matrix != mtx.matrix) {
-			mtx.matrix->counter++;
-			matrix->counter--;
-		}
+		matrixMove(mtx.matrix);
 
 		return mtx;
 	}
 
 };
+
+template <class TypeM>
+class Slice {
+private:
+
+	int height, width, step, startY, startX;
+
+
+public:
+	int index(int i, int j);
+	Matrix<TypeM>* matrix;
+
+	Slice(Matrix<TypeM>* m, int nstartY, int nstartX, int sheight, int swidth, int ntep=1);
+
+	int getWidth();
+	int getHeight();
+	int getStepSize();
+
+	Slice mul(TypeM val);
+	Matrix<TypeM> getAsMatrix();
+
+	static Slice mul(Slice slc, TypeM val);
+	static Matrix<TypeM> getAsMatrix(Slice slc);
+
+
+
+	class SProxy {
+	public:
+
+		Slice* slc;
+		int mi;
+
+		SProxy(Slice* slic, int i) {
+			slc = slic;
+			mi = i;
+		}
+
+		TypeM& operator[](int j) {
+			return slc->matrix->matrix->arr[slc->index(mi, j)];
+		}
+	};
+
+	SProxy operator[](int i) {
+		return SProxy(this, i);
+	}
+
+
+	//exception class
+	class Exception: public std::exception {
+
+	public:
+
+		std::string errorInfo;
+
+		Exception(std::string info) {
+			errorInfo = "Slice Error: " + info;
+		}
+
+		virtual const char* what() const throw() {
+			return errorInfo.c_str();
+		}
+	};
+
+};
+
 
 template <class TypeM>
 Matrix<TypeM> operator+(Matrix<TypeM>& mt1, Matrix<TypeM>& mt2);
@@ -112,5 +233,9 @@ Matrix<TypeM> operator*(TypeM c, Matrix<TypeM>& mt);
 template <class TypeM>
 Matrix<TypeM> operator*(Matrix<TypeM>& mt, TypeM c);
 
-//template <class TypeM>
-//Matrix<TypeM>& operator=(Matrix<TypeM>& mt);
+template <class TypeM>
+bool operator==(Matrix<TypeM> mt1, Matrix<TypeM> mt2);
+
+
+template <class TypeM>
+Slice<TypeM> operator*(Slice<TypeM>& slc, TypeM c);
